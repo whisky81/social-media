d6e5e28f9a474725227600555dc4a3912d46683f
whisky81 <anh666666anh@gmail.com>
2025-01-04 16:18:36 +0700

    add cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 # from .db import create_db_and_tables
 from .routers import post, user, oauth2, vote 
-
+from fastapi.middleware.cors import CORSMiddleware 
 app = FastAPI()
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
 
 # @app.on_event("startup")
 # def on_startup():
